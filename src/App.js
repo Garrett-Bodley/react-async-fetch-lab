f5fbbf30d703fe39d6b5aa25506ce46c8f685834
Garrett-Bodley <Garrett.Bodley@gmail.com>
2021-03-07 15:07:33 -0500

    commit tree 874502a646f25f91df9fd8dd189954fe391da0ad

--- src/App.js
+++ src/App.js
@@ -1,1 +1,28 @@
 // create your App component here
+import React, {Component} from 'react'
+import Astro from './Astro.js'
+
+export default class App extends Component {
+  
+  state = {
+    astros: []
+  }
+
+  componentDidMount(){
+    fetch('http://api.open-notify.org/astros.json').then(resp => resp.json()).then(json => this.saveAstros(json.people))
+  }
+
+  saveAstros = (people) => {
+    this.setState({astros: people.map(astro => <Astro name={astro.name} craft={astro.craft}/>)})
+  }
+
+  render(){
+    return(
+      <div>
+        <ul>
+          {this.state.astros}
+        </ul>
+      </div>
+    )
+  }
+}
